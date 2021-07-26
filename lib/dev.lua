113fedf0e545b83f83985e93a1c7ed79d77fc55a
James Walker <james.chunho+github@gmail.com>
2021-07-26 19:08:33 +0100

    remove comments from dev.lua

--- lib/dev.lua
+++ lib/dev.lua
@@ -4,16 +4,6 @@ inspect = require "dev.inspect"
 
 function clear() reaper.ShowConsoleMsg("") end
 
--- -- debug printing
--- function p(x)
---     if x == "" then x = " " end
---     if x == nil then x = "nil" end
---     if x == true then x = "true" end
---     if x == false then x = "false" end
---     reaper.ShowConsoleMsg(x)
---     reaper.ShowConsoleMsg("\n")
--- end
-
 -- debug printing
 function p(x)
     local x_str = inspect(x)
