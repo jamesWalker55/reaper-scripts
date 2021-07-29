18a69a86064bb8bece5789af5c142147b235a04d
James Walker <james.chunho+github@gmail.com>
2021-07-29 22:47:35 +0100

    Create menu for KAWA explode actions

---
+++ scripts/menus/midi-explode-menu-kawa.lua
@@ -0,0 +1,13 @@
+-- boiler code to override reaper search paths
+local parent_dir = ({reaper.get_action_context()})[2]:match('^(.+[\\//])scripts')
+package.path = parent_dir .. "?.lua"
+
+require "lib.menu"
+
+local menu_table = {
+    {name = "Explode notes to new item", cmd = "_kawa_MIDI2_SelectedNotes_ToNewMediaItem"},
+    {name = "Explode notes to new track", cmd = "_kawa_MIDI2_SelectedNotes_ToNewTrack"},
+    {name = "Explode notes to items by pitch", cmd = "_kawa_MIDI2_ExplodeSelectedNote_Type2"},
+}
+
+QuickMenu(SECTION_ID.MIDI, menu_table)
