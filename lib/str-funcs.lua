ffac3fb514de7f21cf38568eb81eb74296cdec2b
James Walker <james.chunho+github@gmail.com>
2021-08-16 00:09:46 +0100

    improve string.split function, this now splits multiple separators in sequence with blank strings

--- lib/str-funcs.lua
+++ lib/str-funcs.lua
@@ -1,9 +1,18 @@
--- https://stackoverflow.com/questions/1426954/split-string-in-lua
-string.split = function(input, separator)
-  if separator == nil then separator = "%s" end
-  local t = {}
-  for match in string.gmatch(input, "([^" .. separator .. "]+)") do
-    table.insert(t, match)
+string.split = function(text, separator)
+  local items = {}
+  local i = 1
+  local search_pos = 0
+  while true do
+    local match_pos = text:find(separator, search_pos + 1, true)
+    if match_pos == nil then
+      -- if no match, then it is end of string
+      items[i] = text:sub(search_pos + 1, -1)
+      return items
+    else
+      -- split the text at that point
+      items[i] = text:sub(search_pos + 1, match_pos - 1)
+      i = i + 1
+      search_pos = match_pos
   end
-  return t
+  end
 end
