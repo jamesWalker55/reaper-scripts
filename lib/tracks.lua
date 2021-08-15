3d42c2fbd2692de51c46c16a8aff372fa1b545ca
James Walker <james.chunho+github@gmail.com>
2021-08-16 00:13:16 +0100

    make `want_master` of `tracks.iterSelected` optional

--- lib/tracks.lua
+++ lib/tracks.lua
@@ -3,9 +3,11 @@ local tracks = {}
 --[[
 iterator that returns all selected tracks in the current project. Index starts at 1.
 
-    for i, proj, path in tracks.iterSelected(true) do print(i, proj, path) end
+    for i, track in tracks.iterSelected(true) do print(i, track) end
 ]]
 tracks.iterSelected = function(want_master)
+  if want_master == nil then want_master = true end
+
   local function iter(_, i)
     i = i + 1
     local track = reaper.GetSelectedTrack2(0, i - 1, want_master)
