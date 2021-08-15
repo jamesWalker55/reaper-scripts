89ec8d05ed2e52ca9c08b255d0de0015f36fe99a
James Walker <james.chunho+github@gmail.com>
2021-08-16 00:11:28 +0100

    add some clipboard helper functions in `lib.dev`

--- lib/dev.lua
+++ lib/dev.lua
@@ -1,6 +1,7 @@
 -- functions for easier debugging and testing
 
 inspect = require "dev.inspect"
+json = require "dev.json"
 
 function clear() reaper.ShowConsoleMsg("") end
 
@@ -9,4 +10,16 @@ function p(x)
   local x_str = inspect(x)
   reaper.ShowConsoleMsg(x_str)
   reaper.ShowConsoleMsg("\n")
+end
+
+function copy(obj)
+  if type(obj) == "string" then
+    reaper.CF_SetClipboard(obj)
+  else
+    reaper.CF_SetClipboard(json.encode(obj))
+  end
+end
+
+function paste()
+  return reaper.CF_GetClipboard()
 end
